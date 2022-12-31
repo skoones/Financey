1e37b042660dc19428650092a42aabc45d774e06
skoones <juliaskoneczna@gmail.com>
2022-12-31 21:20:00 +0100

    Add Arrow Core library

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.mapstruct:mapstruct:1.5.3.Final")
     implementation("org.mapstruct:mapstruct-processor:1.5.3.Final")
+    implementation("io.arrow-kt:arrow-core:1.1.2")
     kapt("org.mapstruct:mapstruct-processor:1.5.3.Final")
 }
 
