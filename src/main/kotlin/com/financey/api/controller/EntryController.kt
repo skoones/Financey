af858f50ddc857d059600d97b5fa12c5beffb939
skoones <juliaskoneczna@gmail.com>
2023-01-09 02:13:58 +0100

    Handle deleting entries by id

--- src/main/kotlin/com/financey/api/controller/EntryController.kt
+++ src/main/kotlin/com/financey/api/controller/EntryController.kt
@@ -5,14 +5,13 @@ import com.financey.domain.service.EntryService
 import org.openapitools.api.EntryApi
 import org.openapitools.model.EntryDTO
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class EntryController(
     @Autowired private val entryService: EntryService,
-    @Autowired private val entryDtoMapper: EntryDtoMapper,
+    @Autowired private val entryDtoMapper: EntryDtoMapper
 ) : EntryApi {
 
     override fun addEntry(entryDTO: EntryDTO): ResponseEntity<String> {
@@ -22,9 +21,9 @@ class EntryController(
         return ResponseEntity.ok("Entity saved")
     }
 
-    override fun deleteEntriesByIds(ids: List<String>): ResponseEntity<Unit> {
-        // todo Either with left/right and based on this different HTTP status code in the response
+    override fun deleteEntriesByIds(ids: List<String>): ResponseEntity<String> {
         entryService.delete(ids)
-        return ResponseEntity(HttpStatusCode.valueOf(200))
+        return ResponseEntity.ok("Entities with given ids have been deleted")
     }
+
 }
