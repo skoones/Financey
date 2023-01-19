9d37e6d28379d02de6451469ee7bc8fe87457b32
skoones <juliaskoneczna@gmail.com>
2023-01-20 00:33:44 +0100

    Initial frontend setup

--- financey-frontend/src/app/app.component.ts
+++ financey-frontend/src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  favoriteBudgetsTitle: string = FAVORITE_BUDGETS_TITLE;
-  recentBudgetsTitle: string = RECENT_BUDGETS_TITLE;
 }
-
-// TODO extract to constants file
-const FAVORITE_BUDGETS_TITLE: string = "Favorite budgets";
-const RECENT_BUDGETS_TITLE: string = "Recent budgets";
