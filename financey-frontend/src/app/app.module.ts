b880bd40fa9a421414b15ce21a40449eba0acdec
skoones <juliaskoneczna@gmail.com>
2023-01-20 00:33:44 +0100

    Initial frontend setup

---
+++ financey-frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from "@angular/material/table";
+import { BudgetListComponent } from './budget/budget-list/budget-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BudgetListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
