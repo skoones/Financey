0f9342f5a5c34ff2280db6ed7a7ceaa1143422d0
skoones <juliaskoneczna@gmail.com>
2023-01-20 04:45:13 +0100

    Add simple version of budget list component with fetching budgets from database

--- financey-frontend/src/app/budget/budget-list/budget-list.component.ts
+++ financey-frontend/src/app/budget/budget-list/budget-list.component.ts
@@ -1,19 +1,26 @@
-import {Component, Input} from '@angular/core';
-import {BudgetDTO} from "../../../generated/model/budgetDTO";
+import {Component, Input, OnInit} from '@angular/core';
+import {BudgetDTO} from "../../../generated";
+import {BudgetService} from "../../../generated/api/budget.service";
 
 @Component({
   selector: 'app-budget-list',
   templateUrl: './budget-list.component.html',
   styleUrls: ['./budget-list.component.scss']
 })
-export class BudgetListComponent {
+export class BudgetListComponent implements OnInit {
+
   @Input() title = "Budgets"
   displayedColumns = ['name'];
 
-  budgets: BudgetDTO[] = tempBudgets
+  constructor(private budgetService: BudgetService) {}
+
+  budgets: BudgetDTO[] = []
+
+  ngOnInit(): void {
+    // todo placeholder userId
+    this.budgetService.getUncategorizedBudgets("demo").subscribe(data => {
+      this.budgets = data;
+    });
+  }
 
 }
-
-const tempBudgets: BudgetDTO[] = [
-  {name: 'Travel', investment: false, userId: 'demo'}
-]
