9e05180d125560fd850009f0b9aef223c0777663
skoones <juliaskoneczna@gmail.com>
2023-01-20 02:02:28 +0100

    Start adding budget list component

--- financey-frontend/src/app/app.component.ts
+++ financey-frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  favoriteBudgetsTitle: string = FAVORITE_BUDGETS_TITLE
 }
+
+const FAVORITE_BUDGETS_TITLE: string = "Favorite budgets"
--- financey-frontend/src/app/budget/budget-list/budget-list.component.ts
+++ financey-frontend/src/app/budget/budget-list/budget-list.component.ts
@@ -1,26 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {BudgetDTO} from "../../../generated";
-import {BudgetService} from "../../../generated/api/budget.service";
+import {Component, Input} from '@angular/core';
+import {BudgetDTO} from "../../../generated/model/budgetDTO";
 
 @Component({
   selector: 'app-budget-list',
   templateUrl: './budget-list.component.html',
   styleUrls: ['./budget-list.component.scss']
 })
-export class BudgetListComponent implements OnInit {
-
+export class BudgetListComponent {
   @Input() title = "Budgets"
   displayedColumns = ['name'];
 
-  constructor(private budgetService: BudgetService) {}
-
-  budgets: BudgetDTO[] = []
-
-  ngOnInit(): void {
-    // todo placeholder userId
-    this.budgetService.getUncategorizedBudgets("demo").subscribe(data => {
-      this.budgets = data;
-    });
-  }
+  budgets: BudgetDTO[] = tempBudgets
 
 }
+
+const tempBudgets: BudgetDTO[] = [
+  {name: 'Travel', investment: false, userId: 'demo'}
+]
