8bd46d5df6aa6673d38e819825c89c8535bb65aa
skoones <juliaskoneczna@gmail.com>
2023-01-20 12:14:23 +0100

    Arrange main view using CSS flexbox

--- financey-frontend/src/app/app.module.ts
+++ financey-frontend/src/app/app.module.ts
@@ -7,18 +7,40 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from "@angular/material/table";
 import { BudgetListComponent } from './budget/budget-list/budget-list.component';
 import {HttpClientModule} from "@angular/common/http";
+import { AddEntryComponent } from './entry/add-entry/add-entry.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BudgetListComponent
+    BudgetListComponent,
+    AddEntryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatGridListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
