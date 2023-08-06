eeb9702120b01ea63e2b7a00411ec2afbd4b372d
skoones <juliaskoneczna@gmail.com>
2023-08-06 21:01:39 +0200

    Add tests for ExpenseCalculatorService

--- src/test/kotlin/com/financey/domain/service/ExpenseCalculatorServiceTests.kt
+++ src/test/kotlin/com/financey/domain/service/ExpenseCalculatorServiceTests.kt
@@ -22,7 +22,7 @@ class ExpenseCalculatorServiceTests {
     }
 
     @Test
-    fun `calculates monthly expenses correctly`() = runBlocking {
+    fun `calculates expense balance for period correctly`() = runBlocking {
         // given
         val testData = TestDataLoader.loadMonthlyExpenseTestData()
         val expectedResults: List<BigDecimal> = listOf(BigDecimal(-40), BigDecimal(40), BigDecimal(50), BigDecimal(0))
@@ -30,6 +30,39 @@ class ExpenseCalculatorServiceTests {
         testData.zip(expectedResults).forEach { (entries, expectedResult)->
             // when
             val result = expenseCalculatorService.findBalanceForPeriodFromEntries(
+                entries = entries,
+                startDate = LocalDate.of(2023, Month.JULY, 1),
+                endDate = LocalDate.of(2023, Month.JULY, 5)
+            )
+
+            // then
+            assertEquals(expectedResult, result)
+        }
+    }
+
+    @Test
+    fun `calculates expense sum for categories correctly`() = runBlocking {
+        // given
+        val testData = TestDataLoader.loadCategoryToExpenseSumTestData()
+        val testInput = testData.first
+        val expectedResults = testData.second
+
+        // when
+        val result = expenseCalculatorService.findExpenseSumContexts(testInput)
+
+        // then
+        assertEquals(expectedResults, result)
+    }
+
+    @Test
+    fun `calculates expense sum for period correctly`() = runBlocking {
+        // given
+        val testData = TestDataLoader.loadMonthlyExpenseTestData()
+        val expectedResults: List<BigDecimal> = listOf(BigDecimal(50), BigDecimal(10), BigDecimal(0), BigDecimal(0))
+
+        testData.zip(expectedResults).forEach { (entries, expectedResult)->
+            // when
+            val result = expenseCalculatorService.findExpenseSumForPeriodFromEntries(
                 entries = entries,
                 startDate = LocalDate.of(2023, Month.JULY, 1),
                 endDate = LocalDate.of(2023, Month.JULY, 5)
